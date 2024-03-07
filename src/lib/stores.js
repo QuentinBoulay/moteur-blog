de139491688acc9f4de1b25ee1b6faaf73664472
QuentinBoulay <quentinboulay2000@gmail.com>
2024-03-07 17:16:05 +0100

    Update app without localstorage

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -2,25 +2,11 @@ import { writable, get } from 'svelte/store';
 import initialData from './articles.json';
 
 function createArticlesStore() {
-
-    // Charger les données initiales depuis localStorage s'il y en a, sinon depuis initialData
-    const initialArticles = typeof localStorage !== 'undefined' ? JSON.parse(localStorage.getItem('articles')) : initialData;
-    const { subscribe, set, update } = writable(initialArticles);
-
-    // Sauvegarder les articles dans localStorage à chaque mise à jour
-    subscribe(($articles) => {
-        if (typeof localStorage !== 'undefined') {
-            localStorage.setItem('articles', JSON.stringify($articles));
-        }
-    });
+    const { subscribe, update } = writable(initialData);
 
     return {
         subscribe,
-        set,
-        add: (article) => update(articles => {
-            const newArticles = [...articles, article];
-            return newArticles;
-        }),
+        add: (article) => update(articles => [...articles, article]),
         delete: (id) => update(articles => articles.filter(article => article.id !== id)),
         update: (id, updatedArticle) => update(articles => {
             return articles.map(article => {
@@ -38,3 +24,4 @@ export const articles = createArticlesStore();
 export function getArticles() {
     return get(articles);
 }
+ 
