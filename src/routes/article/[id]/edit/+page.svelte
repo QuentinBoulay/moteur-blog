4b33357212b91c00745b53104b3032c34de40584
sebastien-brnt <90051256+sebastien-brnt@users.noreply.github.com>
2024-03-12 19:23:03 +0100

    Factorisation article edit

--- src/routes/article/[id]/edit/+page.svelte
+++ src/routes/article/[id]/edit/+page.svelte
@@ -2,6 +2,7 @@
     import { page } from '$app/stores';
     import { articles, getCategories } from '../../../../lib/stores';
     import { goto } from '$app/navigation';
+    import { onDestroy } from 'svelte';
 
     let article = {
         title: '',
@@ -12,48 +13,59 @@
 
     let categories = getCategories();
 
+    function findArticle() {
+        return $articles.find(a => a.id == $page.params.id);
+    }
+
     function updateArticle() {
-        let rawArticle = $articles.find(a => a.id == $page.params.id);
-        article = {
-            ...rawArticle,
-            categories: rawArticle.categories.map(category => category.name)
-        };
-        articles.update(article);
+        const index = $articles.findIndex(a => a.id === article.id);
+        if (index !== -1) {
+            articles.update(articles => {
+                articles[index] = article;
+                return articles;
+            });
+        }
         goto(`/`);
     }
+
+    function initialize() {
+        article = findArticle();
+    }
+
+    initialize();
+
+    onDestroy(() => {
+        article = null;
+    });
 </script>
 
-<h1>Article {$page.params.id}</h1>
-
-{#if $articles}
-    {#each $articles as article}
-        {#if article.id == $page.params.id}
-        <label>Title : 
-            <input type="text" size={article.title.length} bind:value={article.title} />
-        </label>
-        <br>
-        <label>Text :
-            <textarea rows="5" cols="40" bind:value={article.text} />
-        </label>
-        <br>
-        <label>Categories :         
-            <select multiple bind:value={article.categories}>
-                {#each categories as category}
-                    <option value={category} selected={article.categories.includes(category.name)} >
-                        {category.name}
-                    </option>
-                {/each}
-            </select>
-        </label>
-        <br>
-        <label>Status :
-            <select bind:value={article.status}>
-                <option value="published">Published</option>
-                <option value="draft">Draft</option>
-                <option value="archived">Archived</option>
-            </select>
-        </label>
-        <button on:click={updateArticle}>Save</button>
-        {/if}
-    {/each}
+{#if article}
+    <h1>Article : {article.title}</h1>
+    <label>Titre :
+        <input type="text" bind:value={article.title} />
+    </label>
+    <br>
+    <label>Texte :
+        <textarea bind:value={article.text} />
+    </label>
+    <br>
+    <label>Categories :
+        <select multiple bind:value={article.categories}>
+            {#each categories as category}
+                <option value={category.name} selected={article.categories.includes(category.name)}>
+                    {category.name}
+                </option>
+            {/each}
+        </select>
+    </label>
+    <br>
+    <label>Statut :
+        <select bind:value={article.status}>
+            <option value="published">Published</option>
+            <option value="draft">Draft</option>
+            <option value="archived">Archived</option>
+        </select>
+    </label>
+    <br>
+    <button on:click={updateArticle}>Enregistrer</button>
 {/if}
