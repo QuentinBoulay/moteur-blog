4c2b522b855858c97142770d8ce713a5db0cdb6b
QuentinBoulay <63411603+QuentinBoulay@users.noreply.github.com>
2024-03-26 14:11:52 +0100

    fix selection checkbox edit

--- src/routes/article/[id]/edit/+page.svelte
+++ src/routes/article/[id]/edit/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import { page } from '$app/stores';
-    import { articles, getCategories } from '../../../../lib/stores';
+    import { articles, categories } from '../../../../lib/stores';
     import { goto } from '$app/navigation';
     import Header from '../../../../lib/templates/Header.svelte';
     import Footer from '../../../../lib/templates/Footer.svelte';
@@ -14,7 +14,11 @@
         status: ''
     };
 
-    let categories = getCategories();
+    let selection = [];
+
+    $: {
+        article.categories = selection;
+    }
 
     function onContentChange(event) {
         article.text = event.detail;
@@ -37,6 +41,7 @@
 
     function initialize() {
         article = findArticle();
+        selection = article.categories;
     }
 
     initialize();
@@ -65,10 +70,10 @@
 
         <div class="form-block categories">
             <label>Categories :
-                {#if categories.length > 0}
-                    {#each categories as category}
+                {#if $categories.length > 0}
+                    {#each $categories as category}
                         <label for={category.name}>
-                            <input type="checkbox" value={category.name} id={category.name} checked={article.categories.includes(category.name)}/>
+                            <input type="checkbox" bind:group={selection} value={category.name} id={category.name} checked={article.categories.includes(category.name)} />
                             {category.name}
                         </label>
                     {/each}
